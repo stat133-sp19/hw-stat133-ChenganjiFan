ebfe71502b66b96a79109eb14bc0b301ef0239cb
chenganjifan <fan.bear@berkeley.edu>
2019-05-03 15:25:38 -0700

    sync local with repo

--- workout2/server.R
+++ workout2/server.R
@@ -1,38 +1,6 @@
-library(shiny)
 library(ggplot2)
+library(shiny)
 library(reshape)
-
-
-ui <- fluidPage(
-  titlePanel("Investing Modalities"),
-  fluidRow(
-    column(4, 
-          sliderInput("initial", "Initial Amount",
-                      min = 0, max = 100000, value = 1000, step = 500),
-          sliderInput("contri", "Annual Contribution",
-                      min = 0, max = 50000, value = 2000, step = 500)
-    ),
-    column(4, 
-           sliderInput("r", "Return Rate (in %)",
-                       min = 0, max = 20, value = 5, step = 0.1),
-           sliderInput("g", "Growth Rate (in %)",
-                       min = 0, max = 20, value = 2, step = 0.1)
-    ),
-    column(4, 
-           sliderInput("y", "Years",
-                       min = 0, max = 50, value = 20, step = 1),
-           selectInput("f", "Facet", choices = list("Yes"=1, "No"=2), selected = 2)
-    )
-  ),
-  fluidRow(
-    mainPanel(
-      h4("Timelines"),
-      plotOutput("plot"),
-      h4("Balances"),
-      tableOutput("table")
-    )
-  )
-)
 
 server <- function(input, output) {
   #graph
@@ -72,7 +40,7 @@ server <- function(input, output) {
         theme_bw()
     }
   })
-
+  
   #table
   output$table <- renderTable({
     year <- 0:input$y
@@ -86,8 +54,5 @@ server <- function(input, output) {
     }
     balances <- data.frame("year"= year, "no_contrib" = no_contrib, "fixed_contrib" = fixed_contrib, "growing_contrib" = growing_contrib)
   })
-
+  
 }
-
-shinyApp(ui = ui, server = server)
-
